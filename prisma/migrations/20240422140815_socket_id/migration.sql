ef3b667ce44d85ee4787a7f3dda273a48601c315
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2024-04-29 21:12:50 -0300

    commit

--- prisma/migrations/20240422140815_socket_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `socketId` to the `Player` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Player" ADD COLUMN     "socketId" TEXT NOT NULL;
