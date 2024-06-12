70efef535cf5c49f4254eb732088e71ff30415a5
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2024-06-12 11:32:12 -0300

    get places

--- src/repository/PlaceRepository.ts
+++ src/repository/PlaceRepository.ts
@@ -14,6 +14,7 @@ class PlaceRepository {
 
     async findAll() {
         return await prismaClient.place.findMany({
+            where: { placeCategoryId: null },
             include: { Professions: true },
             orderBy: { name: "asc" }
         })
